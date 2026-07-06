46630b6594ee726144ae6aed63c8eddfcc010303
1796318918 <1796318918@qq.com>
2026-07-06 13:39:55 +0800

    ZYY改版2

--- data/projects.ts
+++ data/projects.ts
@@ -11,10 +11,10 @@ export type Project = {
 export const projectsData: Project[] = [
   {
     "id": "proj_1775049332705",
-    "name": "Computational Chemistry Tool",
-    "githubUrl": "https://github.com/heiehiehi/Computational_Chemistry_Tool",
-    "description": "该工具本作者使用在Win下的WSL2平台，系统为Ubuntu22，个人使用请依据自己数据进行修改（这些工具只是整合了一些流程）",
-    "icon": "🚀",
-    "tags": ["Gromacs", "RMSF"]
+    "name": "Selenium Pytest 自动化测试框架",
+    "githubUrl": "https://github.com/1796318918/Selenium_Pytest_Framework.git",
+    "description": "基于 Selenium WebDriver 和 Pytest 构建的 Web 自动化测试框架，采用页面对象模型（POM）设计，通过 YAML 文件统一管理页面元素定位信息，封装了显式等待、日志记录和失败自动截图功能，并集成 pytest-html 生成直观的 HTML 测试报告，便于快速定位问题和持续集成。",
+    "icon": "🧪",
+    "tags": ["Python", "Selenium", "Pytest", "自动化测试"]
   },
 ];
